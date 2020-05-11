72a3e964413cce15fe89cf578b9890532ad41602
Adriane Justine Tan <eidoriantan@gmail.com>
2020-05-11 17:28:13 +0800

    feat: Transforms HTML tags to Messenger formats

--- src/translate.js
+++ src/translate.js
@@ -5,6 +5,23 @@ const { parseLang } = require('./language.js')
 const DEBUG = process.env.DEBUG || false
 
 /**
+ *  Transforms HTML markups to its respective Messenger format
+ *
+ *    @param {string} text    The text to transform from
+ *    @return {string} transformed
+ */
+function transformHTML (text) {
+  const tags = {
+    em: '_',
+    b: '*'
+  }
+
+  const regexStr = `</?(${Object.keys(tags).join('|')})>`
+  const regex = new RegExp(regexStr, 'g')
+  return text.replace(regex, ($0, $1) => tags[$1])
+}
+
+/**
  *  Translates the text by contacting Google's Translate API.
  *
  *    @param {string} text    The text to be translated
@@ -19,6 +36,7 @@ async function translate (text, language) {
   let translated = `Translated to (${name}): ${result.text}\r\n`
 
   if (result.from.text.didYouMean || result.from.text.autoCorrected) {
+    result.from.text.value = transformHTML(result.from.text.value)
     translated += `\r\nDid you mean, "${result.from.text.value}"?`
   }
 
