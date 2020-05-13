1300dedf7f6ad7244aa1d6bd30757049f8f29bbe
Adriane Justine Tan <eidoriantan@gmail.com>
2020-05-13 19:02:48 +0800

    docs: Add comments to page tools

--- src/page/update-db.js
+++ src/page/update-db.js
@@ -7,12 +7,17 @@ const DB_API_KEY = process.env.DB_API_KEY
 const ACCESS_TOKEN = process.env.ACCESS_TOKEN
 const FB_ENDPOINT = 'https://graph.facebook.com'
 
+/**
+ *  Updating the users on the database with their names
+ */
 async function updateUsers () {
   const headers = { 'X-APIKEY': DB_API_KEY }
   const response = await request('GET', DB_ENDPOINT, headers)
 
   console.log('All users was retrieved')
   response.body.forEach(async user => {
+    if (user.name) return
+
     console.log('Getting user: ' + user.psid)
     const url =
       `${FB_ENDPOINT}/${user.psid}?fields=name&access_token=${ACCESS_TOKEN}`
