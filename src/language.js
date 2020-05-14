78b973089666a62a50fc81a22911a8c0989a5f30
Adriane Justine Tan <eidoriantan@gmail.com>
2020-05-14 23:52:12 +0800

    feat: Initial persistent menu

--- src/language.js
+++ src/language.js
@@ -1,17 +1,17 @@
 
 const languages = require('./languages.js')
 const userDB = require('./user-database.js')
+const { setUserMenu } = require('./menu.js')
 
 /**
  *  Changes the language of the user from the database if supported
  *
- *    @param {string} psid    User-scoped page ID
+ *    @param {string} user    User's object from the database
  *    @param {string} lang    Name of the language
  *    @return {string} message
  */
-async function changeLanguage (psid, lang) {
+async function changeLanguage (user, lang) {
   let name, code
-
   Object.keys(languages).forEach(key => {
     const language = languages[key]
     if (language.regex.exec(lang) !== null) {
@@ -20,10 +20,19 @@ async function changeLanguage (psid, lang) {
     }
   })
 
-  if (name && code) {
-    await userDB.setUser(psid, { language: code })
-    return `Language was changed to ${name}!`
-  } else return `Unknown language: ${lang}`
+  if (!name || !code) return `Unknown language: ${lang}`
+
+  let menu
+  if (code !== user.menu[0]) {
+    menu = [code, user.menu[0], '_help']
+    if (user.stats[code]) user.stats[code].count++
+    else user.stats[code] = { count: 1 }
+  }
+
+  await userDB.setUser(user.psid, { language: code, menu, stats: user.stats })
+  await setUserMenu(user.psid, menu)
+
+  return `Language was changed to ${name}!`
 }
 
 module.exports = { changeLanguage }
