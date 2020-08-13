88f4267ac4bfb06fbd5f5649b37a6abdd1db526d
Adriane Justine Tan <eidoriantan@gmail.com>
2020-08-13 16:25:10 +0800

    chore(version): up version

--- assets/js/xfbml.js
+++ assets/js/xfbml.js
@@ -15,7 +15,7 @@ window.fbAsyncInit = function () {
     version: 'v7.0'
   })
 
-  FB.AppEvents.setAppVersion('3.12.1')
+  FB.AppEvents.setAppVersion('3.13.0')
 
   $('[data-fbml]').each(function () {
     const element = $(this)
