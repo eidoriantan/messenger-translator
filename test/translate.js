b87c01fc97c46464ea88658a634d1d75e97e6861
Adriane Justine Tan <eidoriantan@gmail.com>
2020-08-16 17:59:16 +0800

    test: update `translate` test parameters

--- test/translate.js
+++ test/translate.js
@@ -22,12 +22,12 @@ const translate = require('../src/translate.js')
 
 describe('Translator', () => {
   it('Translate text', async () => {
-    const result = await translate('hello', 'ja', 'en')
+    const result = await translate('hello', 'ja', '', 'en')
     result.includes('こんにちは').should.be.true()
   })
 
   it('Change locale', async () => {
-    const result = await translate('hello', 'ja', 'tl')
+    const result = await translate('hello', 'ja', '', 'tl')
     result.should.endWith('Para sa tulong, i-type "--help"')
   })
 })
