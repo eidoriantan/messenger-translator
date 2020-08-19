e143bd54fdfa031d97173e57c6b2161cdf24a137
Adriane Justine Tan <eidoriantan@gmail.com>
2020-08-19 13:00:10 +0800

    perf: fix SQL syntax [skip ci]

--- setup/database.sql
+++ setup/database.sql
@@ -21,7 +21,7 @@
  *  Setting up a table in a database
  */
 CREATE TABLE users (
-  psid NVARCHAR(16) NOT NULL PRIMARY KEY,
+  psid NVARCHAR(16) NOT NULL,
   name NVARCHAR(255) NOT NULL,
   language NVARCHAR(16) NOT NULL,
   locale NVARCHAR(16) NOT NULL,
