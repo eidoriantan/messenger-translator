826965f566a13fdb8004ba31e3883179e04ab346
Adriane Justine Tan <eidoriantan@gmail.com>
2020-08-28 19:07:48 +0800

    perf: use `use` router instead when validating

--- server.js
+++ server.js
@@ -55,23 +55,7 @@ app.use((req, res, next) => {
   next()
 })
 
-app.get('/webhook', (req, res) => {
-  const mode = req.query['hub.mode']
-  const verifyToken = req.query['hub.verify_token']
-  const challenge = req.query['hub.challenge']
-
-  if (mode === 'subscribe' && verifyToken === VALIDATION_TOKEN) {
-    res.status(200).send(challenge)
-  } else {
-    logger.write('Mode/verification token doesn\'t match', 1)
-    logger.write('Parameters:', 1)
-    logger.write({ mode, verifyToken, challenge }, 1)
-
-    res.status(403).send('Mode/verification token doesn\'t match')
-  }
-})
-
-app.post('/webhook', express.json({
+app.use('/webhook', express.json({
   verify: (req, res, buf) => {
     const signature = req.get('x-hub-signature')
     if (!signature) throw new Error('No signature')
@@ -92,6 +76,22 @@ app.post('/webhook', express.json({
     }
   }
 }))
+
+app.get('/webhook', (req, res) => {
+  const mode = req.query['hub.mode']
+  const verifyToken = req.query['hub.verify_token']
+  const challenge = req.query['hub.challenge']
+
+  if (mode === 'subscribe' && verifyToken === VALIDATION_TOKEN) {
+    res.status(200).send(challenge)
+  } else {
+    logger.write('Mode/verification token doesn\'t match', 1)
+    logger.write('Parameters:', 1)
+    logger.write({ mode, verifyToken, challenge }, 1)
+
+    res.status(403).send('Mode/verification token doesn\'t match')
+  }
+})
 
 app.post('/webhook', (req, res) => {
   const data = req.body
