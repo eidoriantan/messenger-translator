85f1b4239cfe89a2374b16b934a8fd7965533e83
Adriane Justine Tan <eidoriantan@gmail.com>
2020-10-16 23:12:22 +0800

    ci: export `mssql-tools/bin`

--- setup/ci.sh
+++ setup/ci.sh
@@ -17,9 +17,7 @@ curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
 sudo apt-get update
 sudo apt-get install mssql-tools unixodbc-dev
-echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
-echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
-source ~/.bashrc
+export PATH="$PATH:/opt/mssql-tools/bin"
 
 # Connect to MSSQL Server
 echo "$id: Connecting to MSSQL Server..."
