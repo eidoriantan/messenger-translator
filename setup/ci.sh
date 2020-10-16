e1d5b2ab327935211e428f683b77d5a60c5d4862
Adriane Justine Tan <eidoriantan@gmail.com>
2020-10-16 23:08:53 +0800

    ci: accept EULA

--- setup/ci.sh
+++ setup/ci.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+id='Script (ci.sh)'
+
 # Install MSSQL Server
+echo "$id: Installing MSSQL Server..."
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 sudo add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubuntu/16.04/mssql-server-2019.list)"
 sudo apt-get update
@@ -9,6 +12,7 @@ sudo /opt/mssql/bin/mssql-conf -n setup accept-eula
 systemctl status mssql-server --no-pager
 
 # Install MSSQL Server CMD Tools
+echo "$id: Installing MSSQL Server CMD Tools..."
 curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
 sudo apt-get update
@@ -18,5 +22,6 @@ echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
 source ~/.bashrc
 
 # Connect to MSSQL Server
+echo "$id: Connecting to MSSQL Server..."
 sqlcmd -S $SERVER -U $USERNAME -P $PASSWORD -Q "CREATE DATABASE $DATABASE"
 sqlcmd -S $SERVER -U $USERNAME -P $PASSWORD -i ./database.sql
