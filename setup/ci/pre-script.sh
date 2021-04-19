04598c7f0f35f1af829000b3cd18bfdfdf97ec4f
Adriane Justine Geneblaza Tan <eidoriantan@gmail.com>
2021-04-20 02:29:28 +0800

    ci: use `ubuntu-20.04`

--- setup/ci/pre-script.sh
+++ setup/ci/pre-script.sh
@@ -21,7 +21,7 @@ script_id='Script (ci.sh)'
 # Install MSSQL Server
 echo "$script_id: Installing MSSQL Server..."
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-sudo add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubuntu/16.04/mssql-server-2019.list)"
+sudo add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubuntu/20.04/mssql-server-2019.list)"
 sudo apt-get update
 sudo apt-get install -y mssql-server
 sudo /opt/mssql/bin/mssql-conf -n setup accept-eula
@@ -30,7 +30,7 @@ systemctl status mssql-server --no-pager
 # Install MSSQL Server CMD Tools
 echo "$script_id: Installing MSSQL Server CMD Tools..."
 curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
+curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
 sudo apt-get update
 sudo apt-get install mssql-tools unixodbc-dev
 export PATH="$PATH:/opt/mssql-tools/bin"
@@ -41,5 +41,5 @@ sudo systemctl restart mssql-server
 
 # Connect to MSSQL Server
 echo "$script_id: Connecting to MSSQL Server..."
-sqlcmd -S $SERVER -U $USERNAME -P $PASSWORD -d $DATABASE -i ./setup/database.sql
+sqlcmd -S $SERVER -U $USERNAME -P $PASSWORD -d $DATABASE -i setup/database.sql
 systemctl status mssql-server --no-pager
